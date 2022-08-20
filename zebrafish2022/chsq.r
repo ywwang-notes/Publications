b0155725a505063c9280a14f6566a78b4980db23
ywwang-notes <ywwang.studio@gmail.com>
2022-08-20 16:07:34 +0800

    Update chsq.r

--- zebrafish2022/chsq.r
+++ zebrafish2022/chsq.r
@@ -1,6 +1,22 @@
 dat = read.table("pub.tsv", header = TRUE)
 
 # https://sphweb.bumc.bu.edu/otlt/MPH-Modules/PH717-QuantCore/PH717-Module8-CategoricalData/PH717-Module8-CategoricalData4.html
-dat.clean = dat[dat["dpa"] == 1, 2:5] 
-dat.clean = dat.clean[apply(dat.clean, 1, sum) > 0,]
-chisq.test(dat.clean,correct=FALSE)
+
+all.results = c()
+
+# with dead fish
+for (dpa in 1:6){
+    dat.clean = dat[dat["dpa"] == dpa, 2:5] 
+    dat.clean = dat.clean[apply(dat.clean, 1, sum) > 0,]
+    all.results[[dpa]] = chisq.test(dat.clean,correct=FALSE)
+}
+
+# without dead fish
+live.results = c()
+# with dead fish
+for (dpa in 1:6){
+    dat.clean = dat[dat["phase"] != 5 & dat["dpa"] == dpa, 2:5]
+    dat.clean = dat.clean[apply(dat.clean, 1, sum) > 0,]
+    dat.clean = dat.clean[]
+    live.results[[dpa]] = chisq.test(dat.clean,correct=FALSE)
+}
